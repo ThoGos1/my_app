21d4148da197d2e63051d216247c745dc243a45e
ThoGos1 <tgosport@gmail.com>
2022-09-13 00:04:03 +0200

    bruh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PythonComponent } from './python/python.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'python', component: PythonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // This is like a python module that lets me make forms
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PythonComponent } from './python/python.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PythonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
